89c0a129ab5728b5326746ef8f7deae484ddfca9
Konstant <koaster@ya.ru>
2024-03-12 10:18:05 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int M = 5;
+int N = 23;
+void outputNumbers(int num1, int num2)
+{
+    if(num2 == num1-1)
+    {
+        return;
+    }
+    outputNumbers(num1, num2 - 1);
+    Console.Write(num2 + " ");
+}
+outputNumbers(M, N);
+
