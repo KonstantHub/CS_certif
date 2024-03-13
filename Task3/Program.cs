89eadbfd433cc84cfb6100c02c0738313e2bb222
Konstant <koaster@ya.ru>
2024-03-13 13:41:46 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[]myArray = {1, 2, 5, 0, 10, 34, 11};
+int myArrayLength = myArray.Length;
+void outputElemtntsBackwards(int[]array, int length)
+{
+    if(length == 0)
+    {
+        return;
+    }
+    int i = length - 1;
+    Console.Write(array[i] + " ");
+    outputElemtntsBackwards(array, length - 1);
+}
+outputElemtntsBackwards(myArray, myArrayLength);
